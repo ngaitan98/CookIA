b8afc04bf1193c2ecacef9acb4ed75e54d8cc172
Diego Ramos <da.ramos@uniandes.edu.co>
2018-09-19 00:53:11 -0500

    Hecha conexión con Watson Assistant, intento de mostrar conversación en UI

--- src/pages/martha/chat.ts
+++ src/pages/martha/chat.ts
@@ -1,74 +1,53 @@
 import { Component } from '@angular/core';
 import { ViewController, NavParams, NavController } from 'ionic-angular';
-
+import { AssistantV1 } from 'watson-developer-cloud'
+import { SpeechToTextV1 } from 'watson-developer-cloud'
+// import { SpeechRecognition } from '@ionic-native/speech-recognition'
 @Component({
   selector: "page-chat",
   templateUrl: 'martha.html'
 })
 export class ChatPage {
 
-  messages: Array<{message: string, user: string }>;
-  text = 'asdfskdjfh';
+  input: string = "";
 
-  constructor(public viewCtrl: ViewController, public params: NavParams, public navCtrl: NavController) {
-    this.messages = [];
-      this.messages.push({
-        message: "Hola, ¿En qué te puedo ayudar?",
-        user: "martha"
-      })
-      this.messages.push({
-        message: "Quiero cocinar algo",
-        user: "user"
-      })
-      this.messages.push({
-        message: "¿Ya tienes pensado qué cocinar o te ayudo a decidir?",
-        user: "martha"
-      })
-      this.messages.push({
-        message: "Ayúdame",
-        user: "user"
-      })
-      this.messages.push({
-        message: "¿Qué ingredientes tienes disponibles?",
-        user: "martha"
-      })
-      this.messages.push({
-        message: "Pollo, pasta, arroz, tomate...",
-        user: "user"
-      })
-      this.messages.push({
-        message: "De acuerdo, déjame ver qué recetas tengo...",
-        user: "martha"
-      })  
+  messages =  new Array;
+
+  responses = new Array;
+
+  assistant = new AssistantV1({
+    username: 'fef7674c-b975-40c8-ae80-b268f7fae27b',
+    password: 'r3k6OGkccEvm',
+    version: '2018-07-10'
+  });
+
+  constructor(public viewCtrl: ViewController, public params: NavParams, public navCtrl: NavController, /*private speechRecogition: SpeechRecognition*/) {
+
+  }
+
+  sendMessage(input : string) {
+    this.messages.push({message: input, user: "user"});
+    this.assistant.message({
+      workspace_id: '0085a1ac-00fb-457a-a85c-3afa3c2ce5e7',
+      input: { 'text': this.input }
+    }, (err, response) => {
+      if (err)
+        console.log('error:', err);
+      else {
+        this.responses.push(response);
+        var str : string
+        response.output.text.forEach(message => {
+          str = message + "\n"
+          // this.messages.push({message : message, user: "martha"})  
+        });
+        this.messages.push({message : str, user : "martha"})
+        console.log(JSON.stringify(this.messages[this.messages.length-1], null, 2))
+        console.log(JSON.stringify(this.responses[this.responses.length-1], null, 2))
+      }
+    });
   }
 
   close() {
     this.navCtrl.pop();
   }
-
-
 }
-
-//   import { Component } from '@angular/core';
-// import { ViewController } from 'ionic-angular';
-
-// @Component({
-//     template: `
-//       <ion-item>
-//       <h3>
-//         Di algo
-//       </h3>
-//       <img src = "assets/imgs/digital_audio.gif">
-//       </ion-item>
-//       <button ion-button color="secondary" round small float-right (click) = "close()">
-//         Enviar
-//       </button>
-//     `
-//   })
-//   export class ListeningPage {
-//     constructor(public viewCtrl: ViewController) {}
-
-//     close() {
-//       this.viewCtrl.dismiss();
-//     }
-//   }
