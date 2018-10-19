e486b40f84bd8085c80c8c7da491ff2637385dda
Diego Ramos <da.ramos@uniandes.edu.co>
2018-10-19 08:41:58 -0500

    Agregados más métodos de peticiones a la api

--- src/providers/recipes/recipes.ts
+++ src/providers/recipes/recipes.ts
@@ -24,12 +24,51 @@ export class RecipesProvider {
     
   }
 
-  public findRecipesName(name: string) {
-    return this.makeHttpRequest(this.apiUrl+ '/recipes/search?instructionsRequired=true&query=' + name)
+  public quickAnswer(question: string) {
+    let q = encodeURIComponent(question);
+    q = q.replace("%20", "+");
+    return this.makeHttpRequest(this.apiUrl + '/recipes/quickAnswer?q=' + q );
+  }
+
+
+  public findRecipes(name: string, diet?: string, excludedIngredients?: string[], intolerances?: string[]) {
+    if(diet === undefined) {
+      diet = ""
+    }
+    let excludedStr = "";
+    if(excludedIngredients !== undefined) {
+      excludedStr = this.arrayToUriList(excludedIngredients);
+    }
+    let intolerancesStr = "";
+    if(intolerances !== undefined) {
+      intolerancesStr = this.arrayToUriList(intolerances);
+    }
+    return this.makeHttpRequest(this.apiUrl + '/recipes/search?diet=' + diet + '&excludedIngredients=' + excludedStr  + '&instructionsRequired=true&intolerances=' + intolerancesStr + '&imitLicense=true&number=20&query=' + name)
+  }
+
+  public findRecipesIngredients(ingredients: string[]) {
+    //fillIngredients: add info about ingredients in each recipe
+    //limitLicence: only show recipes with atribution licence, if those are requested later, they will have instructions
+    //number: number of recipes in response
+    //ranking: maximize used ingredients(1) or minimize missing ingredients(2)
+    return this.makeHttpRequest(this.apiUrl + '/recipes/findByIngredients?fillIngredients=false&ingredients=' + this.formatListUri(ingredients) + "&limitLicence=true&number=20&ranking=2")
+  }
+
+  public summarizeRecipe(recipeId: number) {
+    return this.makeHttpRequest(this.apiUrl + '/recipes/' + recipeId + '/summary')
+  }
+
+  public recipeInstructions(recipeId: number) {
+    //stepBreakdown: break down the recipe steps even more
+    return this.makeHttpRequest(this.apiUrl + '/recipes/' + recipeId + '/analyzedInstructions?stepBreakdown=true')
+  }
+
+  public findSimilarRecipes(recipeId: number) {
+    return this.makeHttpRequest(this.apiUrl + '/recipes/' + recipeId + '/similar')
   }
 
   public filteredRandomRecipes(tags: string[]) {
-    tags = this.removeSpaces(tags);
+    this.formatListUri(tags);
     let tagsStr = '';
     for (let i = 0; i < tags.length; i++) {
       if(i < tags.length - 1){
@@ -56,11 +95,21 @@ export class RecipesProvider {
     });
   }
 
-  private removeSpaces(strings: string[]) {
-    for(let i = 0; i < strings.length; i++) {
-      strings[i] = strings[i].replace(" ", "+");
+  private arrayToUriList(strings: string[]) {
+    let string = '';
+    for (let i = 0; i < strings.length; i++) {
+      if(i < strings.length - 1){
+        string += strings[i] + '&2C';
+      }
+      else {
+        string += strings[i];
+      }
     }
-    return strings
+    return string;
+  }
+
+  private formatListUri(strings: string[]) {
+    return this.arrayToUriList(strings).replace(" ", "+");
   }
 
 }
