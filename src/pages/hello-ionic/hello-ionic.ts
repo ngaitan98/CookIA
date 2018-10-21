069a154763aa24c3d5839257cb4b4710b7c6d0f8
Diego Ramos <da.ramos@uniandes.edu.co>
2018-10-21 13:24:00 -0500

    Arreglos en RecipesProvider y métodos de filtro por tiempo de preparación

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {NavController, NavParams, ModalController, SelectPopover} from 'ionic-angular';
-import {ItemDetailsPage} from '../item-details/item-details';
+import { NavController, NavParams, ModalController, SelectPopover } from 'ionic-angular';
+import { ItemDetailsPage } from '../item-details/item-details';
 import { PopoverController } from 'ionic-angular';
 import { ChatPage } from '../martha/chat'
 import { FilterPage } from '../hello-ionic/filters'
@@ -11,40 +11,59 @@ import { RecipesProvider } from '../../providers/recipes/recipes'
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
-export class HelloIonicPage { 
-    selectedItem: any;
-    filters: any[];
-    data: any;
-    constructor(public navCtrl: NavController, public navParams: NavParams, 
-        public popoverCtrl: PopoverController, public modalCtrl: ModalController,
-        public recipesProvider: RecipesProvider) {
-         this.loadRecipes();
-    }
-    itemTapped(event, item) {
-      this.navCtrl.push(ItemDetailsPage, {
-        item: item
-      });
+export class HelloIonicPage {
+  selectedItem: any;
+  filters: any[];
+  data: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public popoverCtrl: PopoverController, public modalCtrl: ModalController,
+    public recipesProvider: RecipesProvider) {
+      
+    this.loadRecipes();
+
+  }
+  itemTapped(event, item) {
+    this.navCtrl.push(ItemDetailsPage, {
+      item: item
+    });
   }
   presentMartha() {
-      let profileModal = this.modalCtrl.create(ChatPage, { userId: 8675309 });
-      profileModal.present();
+    let profileModal = this.modalCtrl.create(ChatPage, { userId: 8675309 });
+    profileModal.present();
   }
 
-      presentFiletrs(myEvent) {
+  presentFiletrs(myEvent) {
     let popover = this.popoverCtrl.create(FilterPage);
     popover.present({
       ev: myEvent
     });
   }
 
-  filterBy(filters){
-    
+  filterBy(filters) {
+
   }
-  loadRecipes(){
+
+  findRecipes(search: string) {
+    this.recipesProvider.findRecipes(search)
+      .then(recipes => {
+        this.data = recipes;
+        console.log(this.data);
+      });
+  }
+
+  loadRecipes() {
     this.recipesProvider.randomRecipes()
-        .then(recipes => {
-          this.data = recipes;
-          console.log(this.data);
-    });
+      .then(recipes => {
+        this.data = recipes;
+        console.log(this.data);
+      });
+  }
+
+  filterRandomRecipesPreparationTime(minutes: number) {
+    return this.recipesProvider.fiterRandomByPreparationTime(minutes, this.recipesProvider.randomRecipes());
+  }
+
+  filterSearchPreparationTime(minutes: number, search: string) {
+    return this.recipesProvider.filterSearchByPreparationTime(minutes, this.recipesProvider.findRecipes(search));
   }
 }
--- src/providers/recipes/recipes.ts
+++ src/providers/recipes/recipes.ts
@@ -2,119 +2,196 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 /*
-  Generated class for the RecipesProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
+Generated class for the RecipesProvider provider.
+See https://angular.io/guide/dependency-injection for more info on providers
+and Angular DI.
 */
 @Injectable()
 export class RecipesProvider {
 
-  data: any;
+	private types = {
+		'main course': 'main course',
+		'side dish': 'side dish',
+		'dessert': 'dessert',
+		'appetizer': 'appetizer',
+		'salad': 'salad',
+		'bread': 'bread,',
+		'breakfast': 'breakfast',
+		'soup': 'soup',
+		'beverage': 'beverage',
+		'sauce': 'sauce',
+		'drink': 'drink'
+	};
 
-  apiUrl = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com"
+	private diets = {
+		'pescetarian': 'pescetarian',
+		'lacto vegetarian': 'lacto vegetarian',
+		'ovo vegetarian': 'ovo vegetarian',
+		'vegan': 'vegan',
+		'vegetarian': 'vegetarian'
+	};
 
-  options = {headers : new HttpHeaders({
-    'X-Mashape-Key' : 'nWRzp0wBBKmshvLpaNymy5mPvUyHp1HEanjjsncnm323F301Ih',
-    'Accept' : 'application/json'
-  })};
+	private intolerances = {
+		'dairy': 'dairy',
+		'egg': 'egg',
+		'gluten': 'gluten',
+		'peanut': 'peanut',
+		'seasame': 'sesame',
+		'seafood': 'seafood',
+		'shellfish': 'shellfish',
+		'soy': 'soy',
+		'sulfite': 'sulfite',
+		'tree nut': 'tree nut',
+		'wheat': 'wheat'
+	};
 
+	private data: any;
 
-  constructor(public http: HttpClient) {
-    
-  }
+	private apiUrl = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com"
 
-  public findRecipes(query: string, diet?: string, type?: string, excludedIngredients?: string[], intolerances?: string[]) {
-    let uri = this.apiUrl + '/recipes/search?instructionsRequired=true&limitLicense=true&number=20&query=' + encodeURIComponent(query);
-    if(diet !== undefined) {
-      uri += "&diet=" + encodeURIComponent(diet);
-    }
-    if(type !== undefined) {
-      uri + '&type' + encodeURIComponent(type);
-    }
-    if(excludedIngredients !== undefined) {
-      uri += '&excludedIngredients' + this.arrayToUriList(excludedIngredients);
-    }
-    if(intolerances !== undefined) {
-      uri += '&intolerances' + this.arrayToUriList(intolerances);
-    }
-    return this.makeGetRequest(uri);
-  }
+	private options = {
+		headers: new HttpHeaders({
+			'X-Mashape-Key': 'nWRzp0wBBKmshvLpaNymy5mPvUyHp1HEanjjsncnm323F301Ih',
+			'Accept': 'application/json'
+		})
+	};
 
-  public findRecipesIngredients(ingredients: string[]) {
-    //fillIngredients: add info about ingredients in each recipe
-    //limitLicence: only show recipes with atribution licence, if those are requested later, they will have instructions
-    //number: number of recipes in response
-    //ranking: maximize used ingredients(1) or minimize missing ingredients(2)
-    return this.makeGetRequest(this.apiUrl + '/recipes/findByIngredients?fillIngredients=false&ingredients=' + this.formatListUri(ingredients) + "&limitLicence=true&number=20&ranking=2");
-  }
+	constructor(public http: HttpClient) {
 
-  public summarizeRecipe(recipeId: number) {
-    return this.makeGetRequest(this.apiUrl + '/recipes/' + recipeId + '/summary');
-  }
+	}
 
-  public recipeInstructions(recipeId: number) {
-    //stepBreakdown: break down the recipe steps even more
-    return this.makeGetRequest(this.apiUrl + '/recipes/' + recipeId + '/analyzedInstructions?stepBreakdown=true');
-  }
+	public getTypes() {
+		return this.types;
+	}
 
-  public recipeInformation(recipeId: number) {
-    return this.makeGetRequest(this.apiUrl + '/recipes/' + recipeId + '/information');
-  }
+	public getDiets() {
+		return this.diets;
+	}
 
-  public findSimilarRecipes(recipeId: number) {
-    return this.makeGetRequest(this.apiUrl + '/recipes/' + recipeId + '/similar');
-  }
+	public getIntolerances() {
+		return this.intolerances;
+	}
 
-  public quickAnser(question: string) {
-    return this.makeGetRequest(this.apiUrl + '/recipes/quickAnswer?q=' + encodeURIComponent(question));
-  }
+	public findRecipes(query: string, diet?: string, type?: string, excludedIngredients?: string[], intolerances?: string[]) {
+		let uri = this.apiUrl + '/recipes/search?instructionsRequired=true&limitLicense=true&number=20&query=' + encodeURIComponent(query);
+		if (diet !== undefined) {
+			uri += "&diet=" + encodeURIComponent(diet);
+		}
+		if (type !== undefined) {
+			uri + '&type' + encodeURIComponent(type);
+		}
+		if (excludedIngredients !== undefined) {
+			uri += '&excludedIngredients' + this.arrayToUriList(excludedIngredients);
+		}
+		if (intolerances !== undefined) {
+			uri += '&intolerances' + this.arrayToUriList(intolerances);
+		}
+		return this.makeGetRequest(uri);
+	}
 
-  public ingredientSubstituteByName(ingredientName: string) {
-    return this.makeGetRequest(this.apiUrl + '/food/ingredients/substitutes?ingredientName=' + encodeURIComponent(ingredientName));
-  }
+	public findRecipesIngredients(ingredients: string[]) {
+		//fillIngredients: add info about ingredients in each recipe
+		//limitLicence: only show recipes with atribution licence, if those are requested later, they will have instructions
+		//number: number of recipes in response
+		//ranking: maximize used ingredients(1) or minimize missing ingredients(2)
+		return this.makeGetRequest(this.apiUrl + '/recipes/findByIngredients?fillIngredients=false&ingredients=' + this.formatListUri(ingredients) + "&limitLicence=true&number=20&ranking=2");
+	}
 
-  public ingredientSubstituteById(ingredientId: number) {
-    return this.makeGetRequest(this.apiUrl + '/food/ingredients/' + ingredientId + '/substitutes');
-  }
+	public summarizeRecipe(recipeId: number) {
+		return this.makeGetRequest(this.apiUrl + '/recipes/' + recipeId + '/summary');
+	}
 
-  public joke() {
-    return this.makeGetRequest(this.apiUrl + '/food/jokes/random')
-  }
+	public recipeInstructions(recipeId: number) {
+		//stepBreakdown: break down the recipe steps even more
+		return this.makeGetRequest(this.apiUrl + '/recipes/' + recipeId + '/analyzedInstructions?stepBreakdown=true');
+	}
 
-  public filteredRandomRecipes(tags: string[]) {
-    return this.makeGetRequest(this.apiUrl + '/recipes/random?limitLicense=false&number=20&tags=' + this.formatListUri(tags));
-  }
+	public recipeInformation(recipeId: number) {
+		return this.makeGetRequest(this.apiUrl + '/recipes/' + recipeId + '/information');
+	}
 
-  public randomRecipes() {
-    return this.makeGetRequest(this.apiUrl + '/recipes/random?limitLicense=false&number=20&');
-  }
+	public findSimilarRecipes(recipeId: number) {
+		return this.makeGetRequest(this.apiUrl + '/recipes/' + recipeId + '/similar');
+	}
 
-  private makeGetRequest(uri: string) {
-    return new Promise( resolve => {
-      this.http.get(uri, this.options).subscribe(data => {
-        resolve(data);
-      }, err => {
-        console.log(err);
-      });
-    });
-  }
+	public quickAnser(question: string) {
+		return this.makeGetRequest(this.apiUrl + '/recipes/quickAnswer?q=' + encodeURIComponent(question));
+	}
 
-  private arrayToUriList(strings: string[]) {
-    let string = '';
-    for (let i = 0; i < strings.length; i++) {
-      if(i < strings.length - 1){
-        string += strings[i] + ',';
-      }
-      else {
-        string += strings[i];
-      }
-    }
-    return string;
-  }
+	public ingredientSubstituteByName(ingredientName: string) {
+		return this.makeGetRequest(this.apiUrl + '/food/ingredients/substitutes?ingredientName=' + encodeURIComponent(ingredientName));
+	}
 
-  private formatListUri(strings: string[]) {
-    return encodeURIComponent(this.arrayToUriList(strings));
-  }
+	public ingredientSubstituteById(ingredientId: number) {
+		return this.makeGetRequest(this.apiUrl + '/food/ingredients/' + ingredientId + '/substitutes');
+	}
+
+	public joke() {
+		return this.makeGetRequest(this.apiUrl + '/food/jokes/random')
+	}
+
+	public filteredRandomRecipes(tags: string[]) {
+		return this.makeGetRequest(this.apiUrl + '/recipes/random?limitLicense=false&number=20&tags=' + this.formatListUri(tags));
+	}
+
+	public randomRecipes() {
+		return this.makeGetRequest(this.apiUrl + '/recipes/random?limitLicense=false&number=20');
+	}
+
+	public fiterRandomByPreparationTime(minutes: number, promise: Promise<{}>) {
+		let json: any;
+		let recipes = []
+		promise.then(response => {
+			json = response;
+			for (let recipe of json.recipes) {
+				if (recipe.readyInMinutes <= minutes) {
+					recipes.push(recipe);
+				}
+			}
+		});
+		return recipes;
+	}
+
+	public filterSearchByPreparationTime(minutes: number, promise: Promise<{}>) {
+		let json: any;
+		let recipes = []
+		promise.then(response => {
+			json = response;
+			for(let recipe of json.results) {
+				if(recipe.readyInMinutes <= minutes) {
+					recipes.push(recipe);
+				}
+			}
+		});
+		console.log(recipes);
+		return recipes;
+	}
+
+	private makeGetRequest(uri: string) {
+		return new Promise(resolve => {
+			this.http.get(uri, this.options).subscribe(data => {
+				resolve(data);
+			}, err => {
+				console.log(err);
+			});
+		});
+	}
+
+	private arrayToUriList(strings: string[]) {
+		let string = '';
+		for (let i = 0; i < strings.length; i++) {
+			if (i < strings.length - 1) {
+				string += strings[i] + ',';
+			}
+			else {
+				string += strings[i];
+			}
+		}
+		return string;
+	}
+
+	private formatListUri(strings: string[]) {
+		return encodeURIComponent(this.arrayToUriList(strings));
+	}
 
 }
