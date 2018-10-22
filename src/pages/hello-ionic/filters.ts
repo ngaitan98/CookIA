5a12a6ff71c4045cde23282382e9f126603649ce
ngaitan98 <n.gaitan@uniandes.edu.co>
2018-10-22 16:39:55 -0500

    Hola

--- src/pages/hello-ionic/filters.ts
+++ src/pages/hello-ionic/filters.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { ViewController } from 'ionic-angular';
-
+import { HelloIonicPage } from '../hello-ionic/hello-ionic'
 @Component({
     selector: "filters-text",
     templateUrl: 'filters.html' 
   })
   export class FilterPage {
+    vegan: boolean;
+    vegetarian: boolean;
+    cheap: boolean;
+    gluten: boolean;
+    healthy: boolean;
+    diary: boolean;
     constructor(public viewCtrl: ViewController) {}
-  
+    
     close() {
       this.viewCtrl.dismiss();
+    }
+    filter(){
+      console.log(this.vegan);
+      let tags = [];
+      if(this.vegan)
+      {
+        tags.push("vegan");
+      }
     }
   }
